1d3d4dd66bf3409da2299dc759ad6a93a21a530b
D4C3R <d.c.madrigal02@gmail.com>
2024-09-02 12:55:30 -0600

    uwu

--- practica/src/App.jsx
+++ practica/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
     <nav >
     <td className="nav">
+      <tr>inicio</tr>
       <tr>1</tr>
       <tr>2</tr>
       <tr>3</tr>
@@ -17,7 +18,7 @@ function App() {
     </nav>
     <h1>Hola mundo</h1>
       <div>
-        <img src="https://www.ubuy.cr/productimg/?image=aHR0cHM6Ly9pbWFnZXMtbmEuc3NsLWltYWdlcy1hbWF6b24uY29tL2ltYWdlcy9JLzUxTzlJeFYwNnBMLl9TUzQwMF8uanBn.jpg" alt="" />
+        <img src="https://www.ubuy.cr/productimg/?image=aHR0cHM6Ly9pbWFnZXMtbmEuc3NsLWltYWdlcy1hbWF6b24uY29tL2ltYWdlcy9JLzUxTzlJeFYwNnBMLl9TUzQwMF8uanBn.jpg"  alt="" />
       </div>
       
     </>
