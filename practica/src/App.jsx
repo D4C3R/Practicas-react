aac488b67e4e95b6b9858d03c8b3ca20c0106c9f
D4C3R <d.c.madrigal02@gmail.com>
2024-09-02 11:38:17 -0600

    practica

---
+++ practica/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './app.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <nav >
+    <td className="nav">
+      <tr>1</tr>
+      <tr>2</tr>
+      <tr>3</tr>
+      <tr>4</tr>
+      <tr>5</tr>
+    </td>
+    </nav>
+    <h1>Hola mundo</h1>
+      <div>
+        <img src="https://www.ubuy.cr/productimg/?image=aHR0cHM6Ly9pbWFnZXMtbmEuc3NsLWltYWdlcy1hbWF6b24uY29tL2ltYWdlcy9JLzUxTzlJeFYwNnBMLl9TUzQwMF8uanBn.jpg" alt="" />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
